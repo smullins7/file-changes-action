dc315c92f655acbcfdabbe5cddd0acdb1f2e56e2
Stephen Mullins <stephen.mullins+github@tempus.com>
2021-03-16 16:47:44 -0500

    dummy first

--- src/FilesHelper.ts
+++ src/FilesHelper.ts
@@ -45,7 +45,8 @@ export function sortChangedFiles(files: GitHubFile[]): ChangedFiles {
 }
 
 export function filterChangedFiles(files: GitHubFile[], paths: string[], ignorePaths: string[]): GitHubFile[] {
-  return files.filter((file: GitHubFile, index: number, array: GitHubFile[]): boolean => paths.some(path => file.filename.match(path)) && !ignorePaths.some(ignorePath => file.filename.match(ignorePath)))
+  return files
+  //return files.filter((file: GitHubFile, index: number, array: GitHubFile[]): boolean => paths.some(path => file.filename.match(path)) && !ignorePaths.some(ignorePath => file.filename.match(ignorePath)))
 }
 
 /**
